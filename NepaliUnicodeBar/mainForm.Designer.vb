09311676ccb492fb9a6720a3ff5b2a82d5290d85
Kiran Pantha <info@kiranpantha.com.np>
2020-03-28 21:51:03 +0545

    New Files
    Added by Kiran

---
+++ NepaliUnicodeBar/mainForm.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(mainForm))
+        Me.exitBtn = New System.Windows.Forms.Button()
+        Me.setTmer = New System.Windows.Forms.Timer(Me.components)
+        Me.notify = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.notifyMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.RunAtStartupToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RunAtStartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DonotRunAtStartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CheckStatusToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.sysInfo = New System.Windows.Forms.Label()
+        Me.langLbl = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.notifyMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'exitBtn
+        '
+        Me.exitBtn.BackColor = System.Drawing.Color.Red
+        Me.exitBtn.FlatAppearance.BorderSize = 0
+        Me.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.exitBtn.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.exitBtn.ForeColor = System.Drawing.Color.White
+        Me.exitBtn.Location = New System.Drawing.Point(0, -6)
+        Me.exitBtn.Margin = New System.Windows.Forms.Padding(0)
+        Me.exitBtn.Name = "exitBtn"
+        Me.exitBtn.Size = New System.Drawing.Size(55, 49)
+        Me.exitBtn.TabIndex = 0
+        Me.exitBtn.Text = "X"
+        Me.exitBtn.UseVisualStyleBackColor = False
+        '
+        'setTmer
+        '
+        Me.setTmer.Enabled = True
+        Me.setTmer.Interval = 500
+        '
+        'notify
+        '
+        Me.notify.ContextMenuStrip = Me.notifyMenu
+        Me.notify.Icon = CType(resources.GetObject("notify.Icon"), System.Drawing.Icon)
+        Me.notify.Text = "NotifyIcon1"
+        Me.notify.Visible = True
+        '
+        'notifyMenu
+        '
+        Me.notifyMenu.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.notifyMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RunAtStartupToolStripMenuItem, Me.AboutToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.notifyMenu.Name = "notifyMenu"
+        Me.notifyMenu.Size = New System.Drawing.Size(127, 82)
+        '
+        'RunAtStartupToolStripMenuItem
+        '
+        Me.RunAtStartupToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RunAtStartToolStripMenuItem, Me.DonotRunAtStartToolStripMenuItem, Me.CheckStatusToolStripMenuItem})
+        Me.RunAtStartupToolStripMenuItem.Name = "RunAtStartupToolStripMenuItem"
+        Me.RunAtStartupToolStripMenuItem.Size = New System.Drawing.Size(126, 24)
+        Me.RunAtStartupToolStripMenuItem.Text = "Startup"
+        '
+        'RunAtStartToolStripMenuItem
+        '
+        Me.RunAtStartToolStripMenuItem.Name = "RunAtStartToolStripMenuItem"
+        Me.RunAtStartToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.RunAtStartToolStripMenuItem.Text = "Run at Start"
+        '
+        'DonotRunAtStartToolStripMenuItem
+        '
+        Me.DonotRunAtStartToolStripMenuItem.Name = "DonotRunAtStartToolStripMenuItem"
+        Me.DonotRunAtStartToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.DonotRunAtStartToolStripMenuItem.Text = "Donot Run at Start"
+        '
+        'CheckStatusToolStripMenuItem
+        '
+        Me.CheckStatusToolStripMenuItem.Name = "CheckStatusToolStripMenuItem"
+        Me.CheckStatusToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.CheckStatusToolStripMenuItem.Text = "Check Status"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(126, 24)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(123, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(126, 24)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'sysInfo
+        '
+        Me.sysInfo.AutoSize = True
+        Me.sysInfo.Font = New System.Drawing.Font("Nirmala UI", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.sysInfo.ForeColor = System.Drawing.Color.White
+        Me.sysInfo.Location = New System.Drawing.Point(58, 4)
+        Me.sysInfo.Name = "sysInfo"
+        Me.sysInfo.Size = New System.Drawing.Size(243, 38)
+        Me.sysInfo.TabIndex = 1
+        Me.sysInfo.Text = "नेपाली Unicode Bar"
+        '
+        'langLbl
+        '
+        Me.langLbl.AutoSize = True
+        Me.langLbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.langLbl.ForeColor = System.Drawing.Color.White
+        Me.langLbl.Location = New System.Drawing.Point(346, 5)
+        Me.langLbl.Name = "langLbl"
+        Me.langLbl.Size = New System.Drawing.Size(47, 32)
+        Me.langLbl.TabIndex = 2
+        Me.langLbl.Text = "...."
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Red
+        Me.Panel1.Location = New System.Drawing.Point(326, -8)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(5, 70)
+        Me.Panel1.TabIndex = 3
+        '
+        'mainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.ClientSize = New System.Drawing.Size(549, 40)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.langLbl)
+        Me.Controls.Add(Me.sysInfo)
+        Me.Controls.Add(Me.exitBtn)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "mainForm"
+        Me.Opacity = 0.9R
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "NepaliUnicodeBar"
+        Me.TopMost = True
+        Me.notifyMenu.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents exitBtn As Button
+    Friend WithEvents setTmer As Timer
+    Friend WithEvents notify As NotifyIcon
+    Friend WithEvents sysInfo As Label
+    Friend WithEvents langLbl As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents notifyMenu As ContextMenuStrip
+    Friend WithEvents RunAtStartupToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RunAtStartToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DonotRunAtStartToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents CheckStatusToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ NepaliUnicodeBar/mainForm.vb
@@ -0,0 +1,221 @@
+﻿Imports System.ComponentModel
+Imports System.Security.Permissions
+Imports changeLang
+Public Class mainForm
+    Dim kiLang As New MainWindow
+    Dim workArea = Screen.PrimaryScreen.WorkingArea
+    Private Sub mainForm_load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim args() As String = Environment.GetCommandLineArgs()
+        For Each str As String In args
+            Me.Top = workArea.Height * 1.5
+            Try
+                If (str = "addReg") Then
+                    notify.Visible = False
+                    My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).SetValue(Application.ProductName, Application.ExecutablePath)
+                    MsgBox("The Program now STARTS at StartUp", vbOKOnly + vbInformation, "Runs at Startup")
+                    Me.Close()
+                ElseIf (str = "delReg") Then
+                    notify.Visible = False
+                    My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).DeleteValue(Application.ProductName)
+                    MsgBox("The Program now Doesnot STARTS at StartUp", vbOKOnly + vbExclamation, "NO Startup")
+                    Me.Close()
+                ElseIf (str = "checkReg") Then
+                    notify.Visible = False
+                    My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).DeleteValue(Application.ProductName)
+                    My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).SetValue(Application.ProductName, Application.ExecutablePath)
+                    MsgBox("The Program STARTS at StartUp", vbOKOnly + vbInformation, "Runs at Startup")
+                    Me.Close()
+                End If
+            Catch
+                If (str = "checkReg") Then
+                    notify.Visible = False
+                    MsgBox("The Program Doesnot STARTS at StartUp", vbOKOnly + vbExclamation, "NO Startup")
+                End If
+                Me.Close()
+            End Try
+        Next
+        If (IsProcessRunning(Application.ProductName) > 1) Then
+            notify.Visible = False
+            Me.Activate()
+            MsgBox("The Program doesnot allow multiple Instances.", vbOKOnly + vbExclamation, "Sorry!")
+            Me.Close()
+        End If
+        makeForm()
+        Dim strings() As String = {"आ", "ई", "ऊ", "ऋ", "ऐ", "ओ", "औ", "ज्ञ", "ञ", "क्ष", "ौ", "ॐ"}
+        Dim count As Integer = 0
+        Dim lengthStr = strings.Length - 1
+        For count = (strings.Length - 1) To 0 Step -1
+            Dim lbl = New Button()
+            lbl.Name = "lbl_" & count
+            lbl.Text = strings(count)
+            lbl.Width = 40
+            lbl.Font = New Font("Nirmala", 16, FontStyle.Regular)
+            lbl.FlatStyle = FlatStyle.Flat
+            lbl.BackColor = Color.Black
+            lbl.ForeColor = Color.White
+            lbl.Cursor = Cursors.Hand
+            lbl.Height = 35
+            lbl.Location = New Point(Me.Width - (lengthStr - count + 1) * 45, 2)
+            AddHandler lbl.Click, AddressOf handleClick
+            Me.Controls.Add(lbl)
+        Next count
+    End Sub
+
+
+    Private Sub handleClick(sender As Button, e As EventArgs)
+        SendKeys.Send(sender.Text)
+    End Sub
+    Protected Overrides ReadOnly Property CreateParams() As CreateParams
+        <PermissionSetAttribute(SecurityAction.LinkDemand, Name:="FullTrust")>
+        Get
+            Dim cp As CreateParams = MyBase.CreateParams
+            cp.ExStyle = cp.ExStyle Or CInt(&H8000000L)
+            Return cp
+        End Get
+    End Property
+#Region "Language"
+
+#End Region
+#Region "Make Form"
+    Function makeForm() As Integer
+        Me.BackColor = Color.Black
+        Me.Left = 0
+        Me.Height = 40
+        Me.Width = Screen.PrimaryScreen.WorkingArea.Width
+        Me.Top = Screen.PrimaryScreen.WorkingArea.Height - Me.Height
+        exitBtn.Height = Me.Height
+        exitBtn.Width = 40
+        exitBtn.Top = 0
+        exitBtn.Left = 0
+        exitBtn.Cursor = Cursors.Hand
+        Return 1
+    End Function
+#End Region
+#Region " Move Form "
+
+    ' [ Move Form ]
+    '
+    ' // By Elektro 
+
+    Public MoveForm As Boolean
+    Public MoveForm_MousePosition As Point
+
+    Public Sub MoveForm_MouseDown(sender As Object, e As MouseEventArgs) Handles _
+    MyBase.MouseDown ' Add more handles here (Example: PictureBox1.MouseDown)
+
+        If e.Button = MouseButtons.Left Then
+            MoveForm = True
+            Me.Cursor = Cursors.NoMove2D
+            MoveForm_MousePosition = e.Location
+        End If
+
+    End Sub
+
+    Public Sub MoveForm_MouseMove(sender As Object, e As MouseEventArgs) Handles _
+    MyBase.MouseMove ' Add more handles here (Example: PictureBox1.MouseMove)
+
+        If MoveForm Then
+            Me.Location = Me.Location + (e.Location - MoveForm_MousePosition)
+        End If
+
+    End Sub
+
+    Public Sub MoveForm_MouseUp(sender As Object, e As MouseEventArgs) Handles _
+    MyBase.MouseUp ' Add more handles here (Example: PictureBox1.MouseUp)
+
+        If e.Button = MouseButtons.Left Then
+            MoveForm = False
+            Me.Cursor = Cursors.Default
+        End If
+
+    End Sub
+
+    Private Sub exitBtn_click(sender As Object, e As EventArgs) Handles exitBtn.Click
+        End
+    End Sub
+
+    Private Sub SetTmer_Tick(sender As Object, e As EventArgs) Handles setTmer.Tick
+        'If (InputLanguage.CurrentInputLanguage.Culture.TwoLetterISOLanguageName = "ne") Then
+        ' MsgBox(kiLang._currentLanaguge.TwoLetterISOLanguageName)
+        Try
+            kiLang.HandleCurrentLanguage()
+            If (kiLang._currentLanaguge.TwoLetterISOLanguageName.Contains("ne") = True) Then
+                langLbl.Text = "Nepali"
+                Me.Show()
+                'Me.Top = workArea.Height - 100
+            Else
+                langLbl.Text = "English"
+                Me.Hide()
+                'Me.Top = workArea.Height - Me.Height
+            End If
+        Catch err As Exception
+            //do nothing
+        End Try
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub RunAtStartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RunAtStartToolStripMenuItem.Click
+        Dim procStartInfo As New ProcessStartInfo
+        Dim procExecuting As New Process
+
+        With procStartInfo
+            .Arguments = "addReg"
+            .UseShellExecute = True
+            .FileName = Application.ExecutablePath
+            .WindowStyle = ProcessWindowStyle.Minimized
+            .Verb = "runas" 'add this to prompt for elevation
+        End With
+
+        procExecuting = Process.Start(procStartInfo)
+    End Sub
+
+    Private Sub DonotRunAtStartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DonotRunAtStartToolStripMenuItem.Click
+        Dim procStartInfo As New ProcessStartInfo
+        Dim procExecuting As New Process
+
+        With procStartInfo
+            .Arguments = "delReg"
+            .UseShellExecute = True
+            .FileName = Application.ExecutablePath
+            .WindowStyle = ProcessWindowStyle.Minimized
+            .Verb = "runas" 'add this to prompt for elevation
+        End With
+
+        procExecuting = Process.Start(procStartInfo)
+    End Sub
+
+    Private Sub CheckStatusToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CheckStatusToolStripMenuItem.Click
+        Dim procStartInfo As New ProcessStartInfo
+        Dim procExecuting As New Process
+
+        With procStartInfo
+            .Arguments = "checkReg"
+            .UseShellExecute = True
+            .FileName = Application.ExecutablePath
+            .WindowStyle = ProcessWindowStyle.Minimized
+            .Verb = "runas" 'add this to prompt for elevation
+        End With
+
+        procExecuting = Process.Start(procStartInfo)
+    End Sub
+#End Region
+#Region "Process Twice"
+    Public Function IsProcessRunning(name As String) As Integer
+        'here we're going to get a list of all running processes on
+        'the computer
+        Dim count As Integer
+        For Each clsProcess As Process In Process.GetProcesses()
+            If clsProcess.ProcessName.StartsWith(name) Then
+                'process found so it's running so return true
+                count += 1
+            End If
+        Next
+        'process not found, return false
+        Return count
+    End Function
+#End Region
+End Class
